d9bb166fc6e3a03ec4f4e02574a5b79ddc34b9d8
Roman Matviy <roman@matviy.pp.ua>
2023-04-19 22:32:45 +0300

    add link to repository

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -103,7 +103,7 @@
                 <div class="flex justify-center mt-4 sm:items-center sm:justify-between">
                     <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
                     Створено <a href="https://github.com/MatviyRoman">Roman Matviy</a>,<br>
-                    вихідний код: <link-to-the-git-repo>.
+                    вихідний код: <a href="https://github.com/MatviyRoman/Laravel-gitlab-service">https://github.com/MatviyRoman/Laravel-gitlab-service</a>.
                     </div>
                     <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
                         Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
